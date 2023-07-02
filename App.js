133875c6334b2d56251d7831671f78759f540968
Ambit Biswas <ambit.biswas.whjr@gmail.com>
2023-07-02 17:16:08 +0530

    Fifth Commit for Project 185

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
-import react from "react";
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "./screens/Home";
 import Main from "./screens/Main";
 
-export default function App() {
-  return (
-    <Main />
-  );
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Home" component={Home}/>
+          <Stack.Screen name="Main" component={Main}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
